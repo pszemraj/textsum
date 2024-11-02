13c6f0d38ed83625b212ee50f550a57ae8741e00
peter szemraj <peterszemraj@gmail.com>
2024-11-02 17:26:24 -0400

    📝
    Signed-off-by: peter szemraj <peterszemraj@gmail.com>

--- src/textsum/summarize.py
+++ src/textsum/summarize.py
@@ -26,7 +26,7 @@ from textsum.utils import (
 
 class Summarizer:
     """
-    Summarizer - utility class for summarizing long text using a pretrained model
+    Summarizer - utility class for summarizing long text using a pretrained text2text model
     """
 
     settable_inference_params = [
@@ -60,17 +60,17 @@ class Summarizer:
         f"""
         __init__ - initialize the Summarizer class
 
-        :param str model_name_or_path: the name or path of the model to load, defaults to "BEE-spoke-data/pegasus-x-base-synthsumm_open-16k"
-        :param bool use_cuda: whether to use cuda, defaults to True
+        :param str model_name_or_path: name or path of the model to load, defaults to "BEE-spoke-data/pegasus-x-base-synthsumm_open-16k"
+        :param bool use_cuda: whether to use cuda if available, defaults to True
         :param bool is_general_attention_model: whether the model is a general attention model, defaults to True
-        :param int token_batch_length: the amount of tokens to process in a batch, defaults to 2048
+        :param int token_batch_length: number of tokens to split the text into for batch summaries, defaults to 4096
         :param int batch_stride: the amount of tokens to stride the batch by, defaults to 16
-        :param float max_length_ratio: the ratio of the token_batch_length to use as the max_length for the model, defaults to 0.25
-        :param bool load_in_8bit: whether to load the model in 8bit precision (LLM.int8), defaults to False
-        :param bool compile_model: whether to compile the model (pytorch 2.0+ only), defaults to False
-        :param bool optimum_onnx: whether to load the model in ONNX Runtime, defaults to False
-        :param bool force_cache: whether to force the model to use cache, defaults to False
-        :param bool disable_progress_bar: whether to disable the progress bar, defaults to False
+        :param float max_length_ratio: ratio of the token_batch_length to calculate max_length (of outputs), defaults to 0.25
+        :param bool load_in_8bit: load the model in 8bit precision (LLM.int8), defaults to False
+        :param bool compile_model: compile the model (pytorch 2.0+ only), defaults to False
+        :param bool optimum_onnx: load the model in ONNX Runtime, defaults to False
+        :param bool force_cache: force the model to use cache in generation, defaults to False
+        :param bool disable_progress_bar: disable the per-document progress bar, defaults to False
         :param kwargs: additional keyword arguments to pass to the model as inference parameters, any of: {self.settable_inference_params}
         """
         self.logger = logging.getLogger(__name__)
@@ -113,6 +113,10 @@ class Summarizer:
                 provider=provider,
                 export=not Path(self.model_name_or_path).is_dir(),
             )  # if a directory, already exported
+            self.logger.warning(
+                "ONNXruntime support is experimental, and functionality may vary per-model. "
+                "Model outputs should be checked for accuracy"
+            )
         else:
             self.model = AutoModelForSeq2SeqLM.from_pretrained(
                 self.model_name_or_path,
